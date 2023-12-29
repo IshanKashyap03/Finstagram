3ca80ab9050bf768a6dfd6409fbea8197356ff0f
ishankashyap <ikashyap@ualberta.ca>
2023-12-28 18:54:30 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {BrowserRouter as Router,Route, Redirect, Switch} from 'react-router-dom';
+import Users from './user/pages/users'
+import NewPlace from './places/pages/newplace'
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+
+function App() {
+  return (
+  <Router>
+    <MainNavigation/>
+    {/* this switch statement makes sure that we are not 
+    redirected to "/" if we find a valid path. */}
+    <main>
+      <Switch>
+        <Route path="/" exact>
+          <Users/>
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace/>
+        </Route>
+        <Redirect to="/"/>
+      </Switch>
+    </main>
+  </Router>
+  );
+}
+
+export default App;
---
+++ src/places/pages/newplace.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const newplace = () => {
+    return <h2>New place works!</h2>
+};
+
+export default newplace;
